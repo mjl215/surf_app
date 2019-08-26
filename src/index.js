ebfd6fc94c016c7e7612338e2bb08cd1d9729643
Marcus Lynch <marcusjameslynch@gmail.com>
2019-08-26 21:28:58 +0100

    chnage router order

--- src/index.js
+++ src/index.js
@@ -16,24 +16,22 @@ const port = process.env.PORT
 
 app.use(express.json());
 
+app.use(locationRouter)
 
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
 
 
-// Rotuers
-app.use(locationRouter)
 
-app.get('/favicon.ico', (req, res) =>{
-  res.sendStatus(200)
-})
+
 
 
 app.listen(port, () => {
