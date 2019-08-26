f07b66af27cf94ba41497507ec5dd734c6c40d54
Marcus Lynch <marcusjameslynch@gmail.com>
2019-08-26 20:40:08 +0100

    course 2 changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('./db/mongoose');
 require('./data/magicseaweed_api_call')
 
+const path = require('path')
+
 //import routers
 const locationRouter= require("./routers/location")
 
@@ -14,6 +16,16 @@ const port = process.env.PORT
 
 app.use(express.json());
 
+
+// serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 // Rotuers
 app.use(locationRouter)
 
