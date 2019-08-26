d5fb422877ee049c35356558b472f7f6ea8cbb22
Marcus Lynch <marcusjameslynch@gmail.com>
2019-08-26 21:16:09 +0100

    trying to fix not data showing 2

--- src/index.js
+++ src/index.js
@@ -17,15 +17,17 @@ const port = process.env.PORT
 app.use(express.json());
 
 
-// serve static assets in production
-// if(process.env.NODE_ENV === 'production'){
-//   //set static folder
-//   app.use(express.static('client/build'))
-// }
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'))
+}
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+
 // Rotuers
 app.use(locationRouter)
 
