7c1c5d314107452c30e8bc28b8f30328975f95de
Marcus Lynch <marcusjameslynch@gmail.com>
2019-11-17 23:25:51 +0000

    chache done, but not removal

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+require('../services/cache');
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
--- src/routers/location.js
+++ src/routers/location.js
@@ -4,26 +4,31 @@ const Location = require('../models/location');
 const router = new express.Router();
 
 router.get('/location', async (req, res) => {
-  const redis = require('redis');
-  const redisUrl = 'redis://127.0.0.1:6379';
-  const client = redis.createClient(redisUrl);
-  const util = require('util');
-  client.get = util.promisify(client.get);
+  // const redis = require('redis');
+  // const redisUrl = 'redis://127.0.0.1:6379';
+  // const client = redis.createClient(redisUrl);
+  // const util = require('util');
+  // client.get = util.promisify(client.get);
+
+  // try {
+  //   const cachedLocations = await client.get('all');
+
+  //   if(cachedLocations){
+  //     console.log('from cache');
+  //     return res.send(JSON.parse(cachedLocations));
+  //   }
+
+  //   const locations = await Location.find();
+
+  //   console.log('from db');
+  //   res.send(locations);
+
+  //   client.set('all', JSON.stringify(locations))
 
   try {
-    const cachedLocations = await client.get('all');
-
-    if(cachedLocations){
-      console.log('from cache');
-      return res.send(JSON.parse(cachedLocations));
-    }
-
-    const locations = await Location.find();
-
-    console.log('from db');
+    
+    const locations = await Location.find().cache();
     res.send(locations);
-
-    client.set('all', JSON.stringify(locations))
 
   } catch (error) {
     res.status(500).send();
